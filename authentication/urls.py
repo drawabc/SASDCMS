383de6a9fe1eb53cf7c45ad1b86b238c84dad205
drawabc <echristo001@e.ntu.edu.sg>
2019-04-03 21:23:59 +0800

    profile fix

--- authentication/urls.py
+++ authentication/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^delete/(?P<user_pk>\w+)/$', views.delete_account, name='delete'),
     url(r'^reset_pwd/(?P<user_pk>\w+)/$', views.reset_password, name='reset_pwd'),
-    url(r'^profile/(?P<user_pk>\w+)/$', views.profile, name='profile'),
+    url(r'^profile/$', views.profile, name='profile'),
     url(r'^change_pwd/(?P<user_pk>\w+)/$', views.change_password, name='change_pwd')
 ]
