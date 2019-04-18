df69d053c0c0b85e074e7a97113f525bdf2395c2
naseer2426 <35726629+naseer2426@users.noreply.github.com>
2019-04-19 00:20:01 +0800

    fix test cases

--- CMSApp/tests_views.py
+++ CMSApp/tests_views.py
@@ -2,25 +2,25 @@ from django.test import TestCase
 from django.urls import reverse
 from django.test import Client
 from django.urls import resolve
-from CMSApp.views import *
+import CMSApp.views as cv
 
 class ViewURLsTest(TestCase):
     def test_home_pass(self):
-        expected = home
+        expected = cv.home
         found = resolve('/')
         self.assertEqual(found.func, expected)
 
     def test_input_pass(self):
-        expected = input
+        expected = cv.input
         found = resolve('/input/')
         self.assertEqual(found.func, expected)
 
     def test_detail_pass(self):
-        expected = detail
+        expected = cv.detail
         found = resolve('/detail/')
         self.assertEqual(found.func, expected)
 
     def test_archive_pass(self):
-        expected = archive
+        expected = cv.archive
         found = resolve('/archive/')
         self.assertEqual(found.func, expected)
